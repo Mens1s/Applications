4ec165a1211f6087cdf816e6406e984c1707360f
Ahmet Yigit <82308205+Mens1s@users.noreply.github.com>
2021-09-20 19:04:19 +0300

    Update login.py

--- atm_projects(Live Currency)/AtmProjectV2/login.py
+++ atm_projects(Live Currency)/AtmProjectV2/login.py
@@ -6,9 +6,9 @@ def login(user,password):
             if password == data[user]["password"]:
                 return 1,data[user]["name"],data[user]["surname"],data[user]["tr_balance"],data[user]["currency"]
             else:
-                return 0
+                return 0,0
         else:
-            return 0
+            return 0,0
 def controlUser(user):
     with open("data.json","r") as data:
         data = json.load(data)
