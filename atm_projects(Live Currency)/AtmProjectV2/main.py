cf0ca24d3581a4018286cc00ce933faa6ab30792
Ahmet Yigit <82308205+Mens1s@users.noreply.github.com>
2021-09-25 22:44:53 +0300

    Update main.py

--- atm_projects(Live Currency)/AtmProjectV2/main.py
+++ atm_projects(Live Currency)/AtmProjectV2/main.py
@@ -3,12 +3,13 @@ from PyQt5 import QtWidgets
 from page1 import Ui1_MainWindow
 from page2 import Ui2_MainWindow
 from page3 import Ui3_MainWindow
-from datetime import date, datetime
+from page4 import Ui4_MainWindow
+from page5 import Ui5_MainWindow
+from datetime import datetime
 import currency
 import json
 from login import login
 from login import controlUser
-import time
 
 class App(QtWidgets.QMainWindow):
     def __init__(self):
@@ -138,13 +139,17 @@ class App(QtWidgets.QMainWindow):
         self.ui3.okey.setEnabled(True)
         self.ui3.takerid.setEnabled(True)
         self.ui3.amount_2.setEnabled(True)
-    
+
         self.ui3.okey.clicked.connect(self.controlLoan)
     
     def controlLoan(self):
         if self.ui3.amount_2.text()==self.password_customer:
-            with open("loan.txt","a") as loan:
-                loan.write(str({self.id_customer : self.ui3.takerid.text()}))
+            with open("loan.json","r") as loan:
+                self.oldloan = json.load(loan)
+                self.oldloan[self.id_customer]=self.ui3.takerid.text()
+            with open("loan.json","w") as loan:
+                
+                loan.write(json.dumps(self.oldloan)+"\n")
         
             self.ui3.result.setText("Your wish was sent to manager.")
             self.ui3.okey.setEnabled(False)
@@ -154,12 +159,70 @@ class App(QtWidgets.QMainWindow):
 
         else:
             self.ui3.result.setText("Your password is wrong!")
+            
     def loginasadmin(self):
-        pass
+        self.ui4 = Ui4_MainWindow()
+        self.ui4.setupUi(self)
+
+        self.ui4.pushButton.clicked.connect(self.loginasadmincontrol)
+
+    def loginasadmincontrol(self):
+        if self.ui4.lineEdit.text() == "adminpassword":
+            self.ui5 = Ui5_MainWindow()
+            self.ui5.setupUi(self)
+
+            totalmoney = 0
+            totalloan = 0
+            with open("data.json","r") as money:
+                money = json.load(money)
+                item = ""
+                for b in money:
+                    item = money[b]["name"]+"    "+money[b]["surname"]+"            "+money[b]["tr_balance"]+"                "+money[b]["currency"]["dolar"]
+                    self.ui5.listWidget_2.addItem(item)
+                    item = ""
+                for a in money:
+                    totalmoney = totalmoney + int(money[a]["tr_balance"])
+            
+            with open("loan.json","r") as loan:
+                for a in loan.readlines():
+                    self.loan = loan
+                    totalloan += 1
+                    self.ui5.listWidget.addItem(a)
+                    
+
+            self.ui5.totalmoney.setText(f"Total Money : {totalmoney}")
+            self.ui5.totalrequest.setText(f"Total Loan Requests : {totalloan}")
+
+            self.ui5.pushButton.clicked.connect(self.okeyLoan)
+
+    def okeyLoan(self):
+        if self.ui5.lineEdit_2.text() == "Sure":
+            customer  = self.ui5.lineEdit.text()
+            with open("data.json","r") as old_data:
+                self.old_data = json.load(old_data)
+
+            if customer in self.old_data:
+                with open("loan.json","r") as loan:
+                    loan = json.load(loan)
+                    self.money = int(loan[customer])
+                new_balance = int(self.old_data[customer]["tr_balance"]) + self.money 
+                self.old_data[customer]["tr_balance"] = str(new_balance)
+
+                with open("data.json","w") as new_data:
+                    new_data.write(json.dumps(self.old_data))
+
+                QtWidgets.QMessageBox.about(self,"Exit","Your order succesfully completed... \n You will exit in few seconds..")
+                sys.exit()
+            else:
+                self.ui5.error_label.setText("Wrong id or type !")
+        else:
+            self.ui5.error_label.setText("Wrong id or type !")
+
 def workIt():
     app = QtWidgets.QApplication(sys.argv)
     win = App()
 
     win.show()
     sys.exit(app.exec())
+
 workIt()
